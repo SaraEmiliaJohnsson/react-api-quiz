583f644a63339bb895f1f7a3e12ce5bf072dba88
Sara Johnsson <sara86persson@hotmail.com>
2024-04-22 17:10:17 +0200

    add API categorys to the welcome page

--- react-api-quiz/src/App.tsx
+++ react-api-quiz/src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import Welcome from './components/Welcome'
+
+enum Screen {
+  WELCOME = 'welcome'
+}
 
 function App() {
+  const [currentScreen, setCurrentScreen] = useState<Screen>(Screen.WELCOME);
 
+  let content: React.ReactElement | null = null
+
+  switch (currentScreen) {
+    case Screen.WELCOME:
+      content = <Welcome />
+      break;
+    default:
+      content = null;
+  }
 
   return (
     <>
-
+      {content}
     </>
   )
 }
